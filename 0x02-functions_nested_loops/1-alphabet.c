1e8ffc977593bff08cb2677cf8a366a3bf1fa7b0
Clement Obukoro <cobukoro@gmail.com>
2022-09-22 14:16:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c++);
+	}
+	_putchar('\n');
+}
