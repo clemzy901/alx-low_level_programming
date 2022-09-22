1ee4a362913859b710b8f5f39041f43b69947125
Clement Obukoro <cobukoro@gmail.com>
2022-09-22 15:12:10 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: the sum of both parameter as int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+Footer
+
